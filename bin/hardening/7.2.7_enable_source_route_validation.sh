2864cb87266ec24435f8f3167d7f96869ee38bb3
sielnet <89866652+sielnet@users.noreply.github.com>
2023-08-22 20:13:45 +0200

    Update 7.2.7_enable_source_route_validation.sh

--- bin/hardening/7.2.7_enable_source_route_validation.sh
+++ bin/hardening/7.2.7_enable_source_route_validation.sh
@@ -11,7 +11,7 @@
 set -e # One error, it's over
 set -u # One variable unset, it's over
 
-HARDENING_LEVEL=2
+HARDENING_LEVEL=3
 
 SYSCTL_PARAMS='net.ipv4.conf.all.rp_filter=1 net.ipv4.conf.default.rp_filter=1'
 
