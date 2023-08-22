cbe91a0b897b015dcf124ee75bdba02221c208ee
sielnet <89866652+sielnet@users.noreply.github.com>
2023-08-22 20:11:06 +0200

    Update 7.1.2_disable_send_packet_redirects.sh

--- bin/hardening/7.1.2_disable_send_packet_redirects.sh
+++ bin/hardening/7.1.2_disable_send_packet_redirects.sh
@@ -11,7 +11,7 @@
 set -e # One error, it's over
 set -u # One variable unset, it's over
 
-HARDENING_LEVEL=2
+HARDENING_LEVEL=3
 
 #net.ipv4.conf.all.send_redirects = 0
 #net.ipv4.conf.default.send_redirects = 0
